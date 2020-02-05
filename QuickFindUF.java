94dad05b7921afd9b3031c887e131fd7a7b25d3b
Matheus Gomes <matheusgomes062@gmail.com>
2020-02-05 01:01:31 -0300

    first commit

---
+++ QuickFindUF.java
@@ -0,0 +1,52 @@
+/*
+  Cost Model: Number of array accesses (for read or write).
+  Algorithm: Quick-Find
+
+  Order of growth of number of array access
+  Initialize: N
+  Union: N
+  Find: 1
+
+  Defect: Union too expensive
+  Takes N^2 array access to process sequence of N union commands on N objects.
+*/
+
+import java.time.LocalTime;
+
+public class QuickFindUF {
+  public int[] id;
+
+  public QuickFindUF(int N) {
+    id = new int[N];
+    for (int i = 0; i < N; i++) {
+      id[i] = i;
+    }
+  }
+
+  public boolean connected(int p, int q) {
+    return id[p] == id[q];
+  }
+
+  public void union(int p, int q) {
+    System.out.println("Union type: " + this.getClass().getSimpleName());
+    int pid = id[p];
+    int qid = id[q];
+    for (int i = 0; i < id.length; i++) {
+      if (id[i] == pid) {
+        id[i] = qid;
+      }
+    }
+  }
+
+  public void calculateTimePassedInSeconds(int before) {
+    System.out.println((LocalTime.now().getNano() - before) / 1000000);
+  }
+
+  public static void main(String[] args) {
+    int before = LocalTime.now().getNano();
+    QuickFindUF quickFindUF = new QuickFindUF(2000000);
+    quickFindUF.union(0, 1);
+    quickFindUF.calculateTimePassedInSeconds(before);
+    System.out.println(quickFindUF.id[0]);
+  }
+}
---
+++ QuickUnionUF.java
@@ -0,0 +1,52 @@
+/*
+  Cost Model: Number of array accesses (for read or write).
+  Algorithm: Quick-Union
+
+  Order of growth of number of array access
+  Initialize: N
+  Union: N (include cost of finding roots)
+  Find: N (worst case)
+
+  Defect: Trees can get tall.
+  Find too expensive (could be N array accesses)
+*/
+
+import java.time.LocalTime;
+
+public class QuickUnionUF {
+  private int[] id;
+
+  public QuickUnionUF(int N) {
+    id = new int[N];
+    for (int i = 0; i < N; i++)
+      id[i] = i;
+  }
+
+  private int root(int i) {
+    while (i != id[i])
+      i = id[i];
+    return i;
+  }
+
+  public boolean connected(int p, int q) {
+    return root(p) == root(q);
+  }
+
+  public void union(int p, int q) {
+    int i = root(p);
+    int j = root(q);
+    id[i] = j;
+  }
+
+  public void calculateTimePassedInSeconds(int before) {
+    System.out.println((LocalTime.now().getNano() - before) / 1000000);
+  }
+
+  public static void main(String[] args) {
+    int before = LocalTime.now().getNano();
+    QuickUnionUF quickUnionUF = new QuickUnionUF(2000000);
+    quickUnionUF.union(0, 1);
+    quickUnionUF.calculateTimePassedInSeconds(before);
+    System.out.println(quickUnionUF.id[0]);
+  }
+}
